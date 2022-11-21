39b6f9ec02640c632b2c063c0352838f78501d6f
Sahil Sunil Phadtare <97933138+IamsahilPhadtare@users.noreply.github.com>
2022-11-21 11:27:41 +0530

    Create DSLProject.cpp

---
+++ DSLProject.cpp
@@ -0,0 +1,16 @@
+#inclued<bits/stdc++.h>
+using namespace std;
+ #define MAX 50
+struct phonebook{
+    string name;
+    long tele, mobile;
+    char id[MAX];
+    public:
+    void setData(){
+      string nme=name;
+      long tel=tele, mob=mobile;
+      char _id[MAX]=id[MAX];
+    }
+    
+    
+};
